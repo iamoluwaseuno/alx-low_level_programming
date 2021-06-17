577cbe3cf959f57ad2081f96fed881cd87bb6d3e
Oluwaseun ALX <spreadwebng@gmail.com>
2021-06-17 11:18:20 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - quote
+ * Return:1
+ */
+int main(void)
+{
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch) - 1);
+return (1);
+}
