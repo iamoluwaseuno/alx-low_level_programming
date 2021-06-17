be11526324222b87aec7cebde1719130be57af14
Oluwaseun ALX <spreadwebng@gmail.com>
2021-06-17 10:39:49 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ *main - to print text
+ *return: 0 is return
+ */
 int main(void)
 {
-  printf("\with proper grammar, but the outcome is a piece of art,");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
