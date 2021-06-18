2ae0ee04b11c0a7ccf59aa0c16ae6656526467c6
Oluwaseun ALX <spreadwebng@gmail.com>
2021-06-18 11:12:22 -0700

    update 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,47 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Return: 0
+ * main - main - two pairs of numbers that do not repeat
+ *
+ * Return (0)
  */
 int main(void)
 {
-int c;
-int d;
-int e;
-int f = 0;
-while (f < 10)
+int i, j;
+for (j = 0; j <= 99; j++)
 {
-e = 0;
-while (e < 10)
+for (i = j; i <= 99; i++)
 {
-d = 0;
-while (d < 10)
+if (i != j)
 {
-c = 0;
-while (c < 10)
-{
-if (!(f == c && e == d))
-{
-putchar('0' + f);
-putchar('0' + e);
+putchar(j / 10 + 48);
+putchar(j % 10 + 48);
 putchar(' ');
-putchar('0' + d);
-putchar('0' + c);
-if (!(f + e == 18 && c + d == 17 && d == 9))
+putchar(i / 10 + 48);
+putchar(i % 10 + 48);
+}
+if (j * 100 + i != 9899)
 {
 putchar(',');
 putchar(' ');
 }
 }
-c++;
-d++;
-}
-e++;
-}
-f++;
 }
 putchar('\n');
 return (0);
