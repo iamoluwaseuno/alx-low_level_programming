e6bda9918a3670aea0566e0ecae00bdb34d67563
Oluwaseun ALX <spreadwebng@gmail.com>
2021-06-25 07:51:35 -0700

    updating isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 
 /**
-* _isdigit - Entry point
-*@c: charater to check
-* Return: if is digit  or not, return 1 else 0
-*/
+ * _isdigit - checks for characters that are digits
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-	return (1);
-	else
-	return (0);
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
 }
