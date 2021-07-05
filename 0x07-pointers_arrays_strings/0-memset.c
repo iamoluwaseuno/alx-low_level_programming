99c1cafa33b360c78b3bc8ed61dde9c04c314ae2
Oluwaseun ALX <spreadwebng@gmail.com>
2021-07-05 11:52:21 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * _memset - Fills the first @n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
@@ -9,15 +8,13 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i = 0;
-
-  while (i < n)
-    {
-      s[i] = b;
-      i++;
-    }
-  return (s);
+unsigned int i = 0;
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+return (s);
 }
