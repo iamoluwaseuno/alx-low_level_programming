0c325a4dbf36a13c14aeca43852619c2d5267b2b
Oluwaseun ALX <spreadwebng@gmail.com>
2021-07-07 08:23:37 -0700

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - recurssion of string
+ * @s: string to print
+ *
+ *
+ *
+ */
+void _puts_recursion(char *s)
+{
+  if (*s)
+    {
+      _putchar(*s);
+      _puts_recursion(s + 1);
+    }
+  else
+    {
+      _putchar('\n');
+    }
+}
