f74b33b12f08f1433e8cb6ce3e884f150d5b5e6c
Oluwaseun ALX <spreadwebng@gmail.com>
2021-07-12 09:13:01 -0700

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,67 +1,39 @@
-
-68 lines (64 sloc)  1011 Bytes
-  
-#include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
+#include <stdlib.h>
 /**
- * main - prints the min num of coins to make change for an amount of money
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: int
+ *main - funtion
+ *@argc: legenth of argv
+ *@argv: number of argummet
+ *Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-int count = 0;
+/*Declaring variables*/
+int position, total, change, aux;
+int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+position = total = change = aux = 0;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-else if (atoi(argv[1]) < 0)
+total = atoi(argv[1]); /*Convert str to int*/
+if (total <= 0)
 {
-printf("%d\n", 0);
+printf("0\n");
 return (0);
 }
-count = coin_count(argv, count);
-printf("%d\n", count);
+/*Declaring WHILE*/
+while (coins[position] != '\0')
+{
+if (total >= coins[position])
+{
+aux = (total / coins[position]);
+change += aux;
+total -= coins[position] * aux;
+}
+position++;
+}
+printf("%d\n", change);
 return (0);
-}
-/**
- * coin_count - counts the min coins needed for change
- *
- * @argv: argument vector
- * @count: int to count coins
- * Return: int
- */
-int coin_count(char *argv[], int count)
-{
-int sum = 0;
-while (!(sum + 25 > atoi(argv[1])))
-{
-sum += 25;
-count++;
-}
-while (!(sum + 10 > atoi(argv[1])))
-{
-sum += 10;
-count++;
-}
-while (!(sum + 5 > atoi(argv[1])))
-{
-sum += 5;
-count++;
-}
-while (!(sum + 2 > atoi(argv[1])))
-{
-sum += 2;
-count++;
-}
-while (!(sum + 1 > atoi(argv[1])))
-{
-sum += 1;
-count++;
-}
-return (count);
 }
