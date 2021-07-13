bade67bbe4b64ce71442b78bfdd3824d23c7bea6
iamoluwaseuno <spreadwebng@gmail.com>
2021-07-13 14:50:56 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,43 +6,12 @@
  */
 char **strtow(char *str)
 {
-<<<<<<< HEAD
-	char **ptr;
-	int i, k, len, start, end, j = 0;
-	int words =  countWords(str);
-
-	if (!str || !countWords(str))
-		return (NULL);
-	ptr = malloc(s		start = startIndex(str, j);
-		end = endIndex(str, start);
-		len = end - start;
-		ptr[i] = malloc(sizeof(char) * (len + 1));
-		if (!ptr[i])
-		{
-			i -= 1;
-			while (i >= 0)
-			{
-				free(ptr[i]);
-					i--;
-			}
-			free(ptr);
-			return (NULL);
-		}
-		for (k = 0; k < len; k++)
-			ptr[i][k] = str[start++];
-		ptr[i][k++] = '\0';
-		j = end + 1;
-	}
-	ptr[i] = NULL;
-	return (ptr);
-=======
 int i = 0, j = 0, k = 0;
 int len = 0, count = 0;
 char **f, *col;
 if (!str || !*str)
 {
 return (NULL);
->>>>>>> 17c90fcd8e9b88a8cfe319aa8c959b4531bd2840
 }
 while (*(str + i))
 {
