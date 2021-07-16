78ccf9d91928008171d025f44fc9aa0521810750
Oluwaseun ALX <spreadwebng@gmail.com>
2021-07-15 19:23:42 -0700

    uodate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,29 +16,26 @@ if (s1 == NULL)
 i = 0;
 else
 {
-for (i = 0; s1[i]; i++)
-<<<<<<< HEAD
-	;
-	}
-	if (s2 == NULL)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-			;
-	}
-	if (j > n)
-		j = n;
-	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
-		return (NULL);
-	for (k = 0; k < i; k++)
-		s[k] = s1[k];
-	for (k = 0; k < j; k++)
-		s[k + i] = s2[k];
-	s[i + j] = '\0';
-	return (s);
-=======
+for (i = 0; s1[i]; i++);
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+for (j = 0; s2[j]; j++)
+;
+}
+if (j > n)
+j = n;
+s = malloc(sizeof(char) * (i + j + 1));
+if (s == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+s[k] = s1[k];
+for (k = 0; k < j; k++)
+s[k + i] = s2[k];
+s[i + j] = '\0';
+return (s);
 ;
 }
 if (s2 == NULL)
@@ -59,5 +56,4 @@ for (k = 0; k < j; k++)
 s[k + i] = s2[k];
 s[i + j] = '\0';
 return (s);
->>>>>>> aac45f9abc92ab70a57f3471c29462ee89470d69
 }
