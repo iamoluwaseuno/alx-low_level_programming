91c204dbbd264dcae770aa28f40c86ac3b2ef0f7
iamoluwaseuno <spreadwebng@gmail.com>
2021-08-11 11:12:24 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,22 @@
 #include "holberton.h"
-
 /**
- * create_file - create a file with read/write access for user
- * @filename: name of file to create
- * @text_content: string to write to file
- * Return: 1 on success, -1 on failure
+ * create_file - creates a file
+ *
+ * @filename: file name
+ * @text_content: pointer to text content
+ * Return: 1 on success or -1 otherwise
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, rstatus, i;
-
-if (filename == NULL)
-	return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-if (fd == -1)
-	return (-1);
-
-if (text_content)
-{
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	rstatus = write(fd, text_content, i);
-	if (rstatus == -1)
-		return (-1);
-}
-
+int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600), count = 0;
+if (!filename || fd == -1)
+return (-1);
+while (text_content && text_content[count])
+count++;
+if (!count)
+write(fd, "", count);
+else
+write(fd, text_content, count);
 close(fd);
 return (1);
 }
